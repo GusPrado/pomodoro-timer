d155788e30c7c3293f3ff9c980bc20cc210ea903
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2022-08-24 21:11:50 -0300

    chore: Add theme type & global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/themes/global';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <h2>timer</h2>;
+      <GlobalStyle />
     </ThemeProvider>
   );
 }
